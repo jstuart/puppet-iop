3a06e266c5e20a13ab8348ec90ee7d4a4ee65075
James Stuart <james@stuart.name>
2015-06-11 11:33:53 -0400

    package requires and file perms

--- manifests/ambari/agent.pp
+++ manifests/ambari/agent.pp
@@ -1,8 +1,10 @@
 class iop::ambari::agent (
   
-) {
+) inherits iop::params {
   require iop::users::ambari
   require iop::yum
   
-    package { 'ambari-agent': }
+    package { 'ambari-agent': 
+      require => File[$ambari_repo_file],
+    }
 }
--- manifests/ambari/server.pp
+++ manifests/ambari/server.pp
@@ -1,14 +1,20 @@
 class iop::ambari::server (
   
-) {
+) inherits iop::params {
   require iop::users::ambari
   require iop::yum
   
-  package { 'ambari-server': }
+  package { 'ambari-server': 
+    require => File[$ambari_repo_file],
+  }
 
   $iop_repo_uri = $iop::yum::iop_repo_uri
   $iop_utils_repo_uri = $iop::yum::iop_utils_repo_uri  
   file { '/var/lib/ambari-server/resources/stacks/BigInsights/4.0/repos/repoinfo.xml':
+    owner   => 'root',
+    group   => 'root,',
+    mode    => '0755',
     content => template("${module_name}/var/lib/ambari-server/resources/stacks/BigInsights/4.0/repos/repoinfo.xml.erb"),
+    require => Package['ambari-server'],
   }
 }
