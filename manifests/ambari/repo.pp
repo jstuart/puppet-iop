f4eda411beb3f8cade1658641b3511610ff46309
James Stuart <james@stuart.name>
2015-07-01 19:44:55 -0400

    fixed validate_re order

--- manifests/ambari/repo.pp
+++ manifests/ambari/repo.pp
@@ -74,25 +74,25 @@ define iop::ambari::repo(
 ) {
   
   if $repoid {
-    validate_re($iop::params::repo_name_regex, $repoid, "Invalid repoid: \$repoid='${repoid}'; valid characters are A-Z, a-z, 0-9, _, -, and .")
+    validate_re($repoid, $iop::params::repo_name_regex, "Invalid repoid: \$repoid='${repoid}'; valid characters are A-Z, a-z, 0-9, _, -, and .")
     $real_repoid = $repoid
   } else {
-    validate_re($iop::params::repo_name_regex, $name, "Invalid repoid: \$name='${name}'; specify \$repoid to override; valid characters are A-Z, a-z, 0-9, _, -, and .")
+    validate_re($name, $iop::params::repo_name_regex, "Invalid repoid: \$name='${name}'; specify \$repoid to override; valid characters are A-Z, a-z, 0-9, _, -, and .")
     $real_repoid = $name
   }
   
   if $reponame {
-    validate_re($iop::params::repo_name_regex, $reponame, "Invalid reponame: \$reponame='${reponame}'; valid characters are A-Z, a-z, 0-9, _, -, and .")
+    validate_re($reponame, $iop::params::repo_name_regex, "Invalid reponame: \$reponame='${reponame}'; valid characters are A-Z, a-z, 0-9, _, -, and .")
     $real_reponame = $reponame
   } else {
-    validate_re($iop::params::repo_name_regex, $name, "Invalid reponame: \$name='${name}'; specify \$reponame to override; valid characters are A-Z, a-z, 0-9, _, -, and .")
+    validate_re($name, $iop::params::repo_name_regex, "Invalid reponame: \$name='${name}'; specify \$reponame to override; valid characters are A-Z, a-z, 0-9, _, -, and .")
     $real_reponame = $name
   }
   
   validate_string($baseurl)
   validate_bool($gpgcheck)
   validate_bool($sslverify)
-  validate_re($iop::params::repo_order_regex, $order, "Invalid order: \$order='${order}'; use a numeric value between 10 and 89")
+  validate_re($order, $iop::params::repo_order_regex, "Invalid order: \$order='${order}'; use a numeric value between 10 and 89")
   
   if $iop::ambari_server == true {
     concat::fragment { "iop-server-repo-${name}":
