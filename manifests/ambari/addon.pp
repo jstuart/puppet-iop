74d8125fa4567450e5a558c8847497db90d408dd
James Stuart <james@stuart.name>
2015-07-07 14:04:30 -0400

    require a package name so that we don't try to reinstall on every run.

--- manifests/ambari/addon.pp
+++ manifests/ambari/addon.pp
@@ -8,6 +8,16 @@
 #
 # Parameters:
 #
+# $package_name::                  The name of the package that will be installed.
+#                                  Note that when using an RPM URI (the only
+#                                  option at the moment) the package name MUST
+#                                  match the name of the RPM.  If it does not
+#                                  match you will get failures stating that the
+#                                  package is already installed every time Puppet
+#                                  runs.
+#                                  Required, no default
+#                                  type:string
+#
 # $rpm_uri::                       The URI of the RPM that contains the Ambari
 #                                  metadata.  
 #                                  Required, no default
@@ -33,13 +43,16 @@
 #
 #
 define iop::ambari::addon (
-  $rpm_uri = undef,
-  $ensure  = 'installed',
+  $package_name = undef,
+  $rpm_uri      = undef,
+  $ensure       = 'installed',
 ) {
+  validate_string($package_name)
   validate_string($rpm_uri)
   validate_string($ensure)
 
   package { "${name}-package":
+    name     => $package_name,
     ensure   => $ensure,
     provider => 'rpm',
     source   => $rpm_uri,
